7f8473db4d60d23c8dd86ed57685a395127db413
NasrinLa <nasrin.latifi95@gmail.com>
2019-01-31 10:25:55 +0330

    Add files via upload

---
+++ DataComponent.js
@@ -0,0 +1,19 @@
+export default data=[
+
+
+        {'name': 'Ben', 'id': 1 , 'bio' : "now I'm using whatsapp", 'kind' : 'phone_call' , 'images': require('./accest/profiles/Unknown_Person_Male_W.png')},
+        {'name': 'Susan', 'id': 2 , 'bio' : "now I'm using whatsapp", 'kind' : 'video_call' , 'images': require('./accest/profiles/5.jpg')},
+        {'name': 'Robert', 'id': 3 , 'bio' : "now I'm using whatsapp" , 'kind' : 'phone_call', 'images': require('./accest/profiles/1.jpg')},
+        {'name': 'Mary', 'id': 4 , 'bio' : "now I'm using whatsapp", 'kind' : 'phone_call' , 'images': require('./accest/profiles/Unknown_Person_Male_W.png')},
+        {'name': 'Daniel', 'id': 5 , 'bio' : "now I'm using whatsapp", 'kind' : 'video_call' , 'images': require('./accest/profiles/2.jpg')},
+        {'name': 'Laura', 'id': 6 , 'bio' : "now I'm using whatsapp", 'kind' : 'phone_call' , 'images': require('./accest/profiles/Unknown_Person_Male_W.png')},
+        {'name': 'John', 'id': 7 , 'bio' : "now I'm using whatsapp", 'kind' : 'phone_call' , 'images': require('./accest/profiles/4.jpg')},
+        {'name': 'Debra', 'id': 8 , 'bio' : "now I'm using whatsapp", 'kind' : 'video_call' , 'images': require('./accest/profiles/Unknown_Person_Male_W.png')},
+        {'name': 'Aron', 'id': 9 , 'bio' : "now I'm using whatsapp", 'kind' : 'phone_call' , 'images': require('./accest/profiles/Unknown_Person_Male_W.png')},
+        {'name': 'Ann', 'id': 10 , 'bio' : "now I'm using whatsapp", 'kind' : 'phone_call' , 'images': require('./accest/profiles/6.jpg')},
+        {'name': 'Steve', 'id': 11 , 'bio' : "now I'm using whatsapp", 'kind' : 'video_call' , 'images': require('./accest/profiles/Unknown_Person_Male_W.png')},
+        {'name': 'Olivia', 'id': 12 , 'bio' : "now I'm using whatsapp", 'kind' : 'phone_call' , 'images': require('./accest/profiles/3.jpg')}
+   
+
+
+]
